0a8b7f733a7ec45e0b154ef2a04a34e3c86f074c
govert@882ca7fe-3cf8-0310-95c5-f9aabef819dd <none@none>
2010-08-27 10:09:52 +0000

    Changed bootstrap.py for using multiple profiles
    --HG--
    extra : convert_revision : svn%3A882ca7fe-3cf8-0310-95c5-f9aabef819dd/fcrepo/trunk%4044826

--- bootstrap.py
+++ bootstrap.py
@@ -17,14 +17,42 @@ Simply run this script in a directory containing a buildout.cfg.
 The script accepts buildout command-line options, so you can
 use the -c option to specify an alternate configuration file.
 
-$Id: bootstrap.py 90478 2008-08-27 22:44:46Z georgyberdyshev $
+$Id: bootstrap.py 29371 2008-06-19 16:03:45Z sylvain $
 """
 
-import os, shutil, sys, tempfile, urllib2
+import os, os.path, shutil, sys, tempfile, urllib2
 
 tmpeggs = tempfile.mkdtemp()
 
-is_jython = sys.platform.startswith('java')
+enable_virtualenv = False
+if '--virtualenv' in sys.argv:
+    enable_virtualenv = True
+    sys.argv.remove('--virtualenv')
+
+if '--buildout-profile' in sys.argv:
+    index = sys.argv.index('--buildout-profile') + 1
+    if index > len(sys.argv):
+        raise ValueError, '--buildout-profile require a config file.'
+    buildout_config = sys.argv[index]
+    if not os.path.isfile(buildout_config):
+        raise ValueError, 'no such configuration file.'
+
+    print "Creating configuration '%s'" % os.path.abspath('buildout.cfg')
+    config = open('buildout.cfg', 'w')
+    config.write("""[buildout]
+extends = %s
+""" % buildout_config)
+    del config
+
+    sys.argv.remove('--buildout-profile')
+    sys.argv.remove(buildout_config)
+
+
+if sys.platform.startswith('win'):
+    bin_dir = 'Scripts'
+else:
+    bin_dir = 'bin'
+python_path = os.path.join(bin_dir, os.path.basename(sys.executable))
 
 try:
     import pkg_resources
@@ -36,41 +64,32 @@ except ImportError:
 
     import pkg_resources
 
-if sys.platform == 'win32':
-    def quote(c):
-        if ' ' in c:
-            return '"%s"' % c # work around spawn lamosity on windows
-        else:
-            return c
-else:
-    def quote (c):
-        return c
-
 cmd = 'from setuptools.command.easy_install import main; main()'
-ws  = pkg_resources.working_set
+if sys.platform == 'win32':
+    cmd = '"%s"' % cmd # work around spawn lamosity on windows
 
-if is_jython:
-    import subprocess
-    
-    assert subprocess.Popen([sys.executable] + ['-c', quote(cmd), '-mqNxd', 
-           quote(tmpeggs), 'zc.buildout'], 
-           env=dict(os.environ,
-               PYTHONPATH=
-               ws.find(pkg_resources.Requirement.parse('setuptools')).location
-               ),
-           ).wait() == 0
-
-else:
-    assert os.spawnle(
-        os.P_WAIT, sys.executable, quote (sys.executable),
-        '-c', quote (cmd), '-mqNxd', quote (tmpeggs), 'zc.buildout',
-        dict(os.environ,
-            PYTHONPATH=
-            ws.find(pkg_resources.Requirement.parse('setuptools')).location
-            ),
-        ) == 0
+ws = pkg_resources.working_set
+assert os.spawnle(
+    os.P_WAIT, sys.executable, sys.executable,
+    '-c', cmd, '-mqNxd', tmpeggs, 'zc.buildout', 'virtualenv',
+    dict(os.environ,
+         PYTHONPATH=
+         ws.find(pkg_resources.Requirement.parse('setuptools')).location
+         ),
+    ) == 0
 
 ws.add_entry(tmpeggs)
+if enable_virtualenv and not os.path.isfile(python_path):
+    ws.require('virtualenv')
+    import virtualenv
+    from subprocess import call
+    args = sys.argv[:]
+    sys.argv = [sys.argv[0], os.getcwd(), '--clear', '--no-site-package']
+    virtualenv.main()
+    call(python_path + ' ' + ' '.join(args), shell=True)
+    sys.exit(0)
+    print 'exit'
+
 ws.require('zc.buildout')
 import zc.buildout.buildout
 zc.buildout.buildout.main(sys.argv[1:] + ['bootstrap'])
